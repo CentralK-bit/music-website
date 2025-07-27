544c6c52f94ef684cc32006d70af834c9c0d7d76
CentralK-bit <524c0001@student.tdtu.edu.vn>
2025-07-27 13:51:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const player = document.getElementById("player");
+const playBtn = document.getElementById("play");
+const pauseBtn = document.getElementById("pause");
+
+playBtn.addEventListener("click", () => {
+    player.play();
+});
+
+pauseBtn.addEventListener("click", () => {
+    player.pause();
+});
